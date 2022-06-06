34267f4d2696739969b09255029823808c4909da
= <saedi.behnam.336@gmail.com>
2022-06-06 17:21:05 +0430

    Add model and initial controller

---
+++ api/Saba.Data/Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Saba.Data.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public virtual User Creator { get; set; }
+
+        [ForeignKey(nameof(Creator))]
+        public string CretorId { get; set; }
+    }
+}
