36b82626ecb93531bbdb9bae9ce5ab8689239a17
Behnam <saedi.behnam.336@gmail.com>
2022-06-11 02:08:17 +0430

    Format code

--- web/src/app/course/course.component.spec.ts
+++ web/src/app/course/course.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CourseComponent } from './course.component';
 
-
 describe('CourseComponent', () => {
     let component: CourseComponent;
     let fixture: ComponentFixture<CourseComponent>;
